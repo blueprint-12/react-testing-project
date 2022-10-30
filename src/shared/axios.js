0beefe5d5a42337f89de6a07609ece0e2a22ff29
blueprint-12 <blueprint12@daum.net>
2022-10-30 21:22:56 +0900

    [feat] axios.interceptors 설정

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -3,4 +3,28 @@ import * as gvar from "./global_variables";
 
 export const api = axios.create({
   baseURL: gvar.BASE_URL,
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default api;
