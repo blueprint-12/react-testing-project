cb57e46f147d1e7626cfff03eaaa3e8c79cd8de0
blueprint-12 <blueprint12@daum.net>
2022-11-02 23:56:45 +0900

    [style] 세련된 css작업하는 법 docs 참고 반영 CSS modules 및 Layout 컴포넌트 생성

---
+++ next-blog/components/layout.js
@@ -0,0 +1,80 @@
+import styles from "./layout.module.css";
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+// 추가기능
+/*
+- meta tags (like og:image), which are used to describe a page's content
+- Boolean home prop which will adjust the size of the title and the image
+- “Back to home” link at the bottom if home is false
+- Added images with next/image, which are preloaded with the priority attribute
+*/
+const name = "감자";
+export const siteTitle = "Next.js smaple website";
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          property="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
+        <meta name="og:title" content={siteTitle} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <header className={styles.header}>
+        {home ? (
+          <>
+            <Image
+              priority
+              src="/images/cat.jpg"
+              className={utilStyles.borderCircle}
+              height={144}
+              width={144}
+              alt=""
+            />
+            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <Link href="/">
+              <a>
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt=""
+                />
+              </a>
+            </Link>
+            <h2 className={utilStyles.headingLg}>
+              <Link href="/">
+                <a className={utilStyles.colorInherit}>{name}</a>
+              </Link>
+            </h2>
+          </>
+        )}
+      </header>
+      <main>{children}</main>
+      {!home && (
+        <div className={styles.backToHome}>
+          <Link href="/">
+            <a>← Back to home</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+}
