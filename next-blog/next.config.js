5fb75dadaa65896b65fcf5077ef15b23c0684c2c
blueprint-12 <blueprint12@daum.net>
2022-11-08 23:54:44 +0900

    [fix] node core moudule인 fs 모듈 정상 실행을 위해 next.config 수정 *유의사항- 웹팩5 버전부터는 사용 시, node core의 폴리필  제공X)

--- next-blog/next.config.js
+++ next-blog/next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
   swcMinify: true,
-}
+  //내가 사용하고 있는 웹팩 버전 5임 node core에 대한 폴리필을 제공하지 않으므로
+  //직접 작성해줘야 한다.
+  //참고자료: https://stackoverflow.com/questions/64557638/how-to-polyfill-node-core-modules-in-webpack-5
+  // webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { ...config.resolve.fallback, fs: false };
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
