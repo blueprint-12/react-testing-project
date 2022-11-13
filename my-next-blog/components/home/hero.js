d8e232984d944b878c254c6de4d0a15a7c633346
blueprint-12 <blueprint12@daum.net>
2022-11-13 22:29:55 +0900

    feat: 메인페이지 버튼 /projects 라우팅 처리

--- my-next-blog/components/home/hero.js
+++ my-next-blog/components/home/hero.js
@@ -1,6 +1,8 @@
 import Animation from "../animation";
+import { useRouter } from "next/router";
 
 export default function Hero() {
+  const router = useRouter();
   return (
     <>
       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
@@ -20,7 +22,9 @@ export default function Hero() {
           인간의 눈이 운다.
         </p>
         <div className="flex justify-center">
-          <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+          <button
+            onClick={() => router.push("/projects")}
+            className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             프로젝트 보러가기
           </button>
         </div>
