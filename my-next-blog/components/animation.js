e739a8ca7b4335b22540fa9660725b5d48ec44c6
blueprint-12 <blueprint12@daum.net>
2022-11-16 19:36:14 +0900

    style: 404 페이지 로티 애니메이션 이미지 추가

--- my-next-blog/components/animation.js
+++ my-next-blog/components/animation.js
@@ -5,7 +5,21 @@ import Lottie from "react-lottie-player";
 // import Lottie from 'react-lottie-player/dist/LottiePlayerLight'
 
 import lottieJson from "../public/main-animation.json";
+import lottie404Json from "../public/404-animation.json";
 
+//for 메인페이지
 export default function Animation() {
   return <Lottie loop animationData={lottieJson} play />;
+}
+
+//for 404페이지
+export function NotFoundAnimation() {
+  return (
+    <Lottie
+      loop
+      animationData={lottie404Json}
+      play
+      style={{ width: 300, height: 300 }}
+    />
+  );
 }
--- my-next-blog/pages/404.js
+++ my-next-blog/pages/404.js
@@ -1,9 +1,18 @@
 //404 페이지에 관한 것은 404.js로 커스텀이 가능하다.
+import { NotFoundAnimation } from "../components/animation";
+import Layout from "../components/layout";
 
 export default function PageNotFound() {
   return (
-    <>
-      <h1>페이지를 찾을 수 없습니다.</h1>
-    </>
+    <Layout>
+      <section className="flexs flex-col items-center justify-center text-gray-600 body-font min-h-screen">
+        <div className="container mx-auto flex flex-col items-center justify-center min-h-screen">
+          <h3 className="font-semibold md:text-3xl text-xl  my-1">
+            You lost your way find Home!
+          </h3>
+          <NotFoundAnimation />
+        </div>
+      </section>
+    </Layout>
   );
 }
