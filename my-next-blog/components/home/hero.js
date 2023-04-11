b1aefe35eefc7657c29a1c13131289ebfa1049ed
blueprint-12 <blueprint12@daum.net>
2023-04-11 13:19:01 +0900

    fix: eslint config conflict 해결, 배포 실패 문제 해결

--- my-next-blog/components/home/hero.js
+++ my-next-blog/components/home/hero.js
@@ -9,17 +9,14 @@ export default function Hero() {
         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
           안녕하세요. W.H.입니다.
           <br className="hidden lg:inline-block" />
-          오늘도 빡코딩합시다!
+          즐거운 하루 되세요.
         </h1>
         <p className="mb-8 leading-relaxed">
-          보이는 끓는 사랑의 황금시대를 커다란 끓는다. 가치를 소금이라 무한한
-          충분히 고행을 속잎나고, 곳으로 뿐이다. 이것을 같이, 이상의 것이다.
-          이상은 얼마나 얼마나 싸인 충분히 열매를 가는 위하여서. 불어 온갖
-          실현에 영락과 쓸쓸한 이는 대한 끓는다. 어디 보이는 보이는 발휘하기
-          우리는 못하다 그들에게 쓸쓸하랴? 들어 속에 뜨고, 관현악이며, 갑 길을
-          그들에게 것이다. 더운지라 품고 모래뿐일 사랑의 이상이 품에 우리는
-          그들의 아니다. 하여도 사랑의 보이는 노년에게서 힘차게 있을 청춘은
-          인간의 눈이 운다.
+          한 폴더에 react 프로젝트와 next 프로젝트를 같이 두지마세요! eslint
+          config에러를 마주하게 될 것입니다. 무엇이 문제인 지 모를 때에는 에러
+          코드를 천천히 읽어보시면 보통은 해결이 됩니다. 덕분에 프로젝트
+          세팅하는 법에 대해서 공부하는 계기가 됐습니다. Vercel 배포 실패가 너무
+          많아서 창피스럽습니다. 무시해주세요.
         </p>
         <div className="flex justify-center">
           <button
