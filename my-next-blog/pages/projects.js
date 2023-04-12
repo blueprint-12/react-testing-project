360cd88ba683a03e794c2d8e801d4f4c475ca2da
blueprint-12 <blueprint12@daum.net>
2023-04-12 12:39:20 +0900

    fix: 데이터 undefined 방지 방법 변경: 옵셔널 체이닝 -> &&

--- my-next-blog/pages/projects.js
+++ my-next-blog/pages/projects.js
@@ -49,7 +49,9 @@ export default function Projects({ projects }) {
         </Head>
         <h1 className="text-4xl font-bold sm:text-6xl">
           총 프로젝트 :
-          <span className="pl-4 text-blue-500">{projects?.results.length}</span>
+          <span className="pl-4 text-blue-500">
+            {projects && projects.results.length}
+          </span>
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 m-6 py-10">
           {projects &&
